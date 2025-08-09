96ec03aad89e1086da2af7f62aca1f0531ba712d
Salim <abdulsalim232004@gmail.com>
2025-08-09 07:23:36 +0530

    Deploy changes Check

--- src/Data/projects.js
+++ src/Data/projects.js
@@ -5,7 +5,7 @@ const projects = [
     title: "F2C-Smart",
     tech: ["React JS", "Node JS", "Express JS", "PostgreSQL", "Bootstrap"],
     image: require("../images/F2C.png"),
-    timeline: "2024",
+    timeline: "Apr - 2025 - May - 2025",
     description:
       "A farmer-to-consumer smart platform eliminating middlemen, ensuring fair pricing and fresh produce delivery. Built with the PERN stack for real-time product management, secure transactions, and responsive UI. Designed to empower farmers, streamline logistics, and enhance consumer trust."
   },
@@ -13,9 +13,9 @@ const projects = [
     id: 2,
     sno: "02",
     title: "Saleem's Portfolio",
-    tech: ["React JS", "Node JS", "Gsap", "JavaScript", "Git", "Netlify"],
+    tech: ["React JS", "Node JS", "Gsap", "JavaScript", "Git", "Vercel"],
     image: require("../images/portfolio.png"),
-    timeline: "2025",
+    timeline: "July-2025",
     description:
       "A personal portfolio showcasing my skills, projects, and achievements with a modern, animated interface. Developed using React JS, GSAP for smooth animations, and deployed on Netlify. Focused on clean UI/UX and a responsive, fast-loading experience to impress clients and recruiters."
   },
@@ -25,7 +25,7 @@ const projects = [
     title: "Scheduling App",
     tech: ["React JS", "Node JS", "Express JS", "PostgreSQL", "Git"],
     image: require("../images/SchedulingAPP.png"),
-    timeline: "2024",
+    timeline: "August - 2024",
     description:
       "A resource scheduling web app for Anna University’s CED Makers Lab. Allows users to book resources, manage time slots, and avoid conflicts. Built with the PERN stack for efficiency, security, and ease of use, optimizing lab operations for students and staff."
   },
@@ -35,7 +35,7 @@ const projects = [
     title: "StudioARKH'ives",
     tech: ["Wix Editor", "Wix Studio", "Wix CMS"],
     image: require("../images/STudioAr.png"),
-    timeline: "2024",
+    timeline: "September -2024",
     description:
       "A creative studio website designed to showcase projects, services, and client work. Built with Wix Studio and CMS for rapid deployment, responsive layouts, and easy content updates. Focused on visual appeal, storytelling, and enhancing the brand’s online presence."
   }
